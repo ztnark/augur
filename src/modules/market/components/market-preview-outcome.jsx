acabee3fb6940e5317dab07fbb9d9e62449106b9
JohnAllen <johnjalleniii@gmail.com>
2017-06-04 12:03:01 -0400

    Create new class market-preview-outcome component

---
+++ src/modules/market/components/market-preview-outcome.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import ReactTooltip from 'react-tooltip';
+
+import ValueDenomination from 'modules/common/components/value-denomination';
+
+import { OUTCOME_VALUE_INCREASED, OUTCOME_VALUE_DECREASED } from 'modules/outcomes/constants/outcome-value-change';
+
+import getValue from 'utils/get-value';
+
+export default class MarketPreviewOutcome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lastPricePercentChange: null,
+    };
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    const p = this.props;
+    const lastPricePercent = getValue(p, 'outcome.lastPricePercent');
+    const nextPricePercent = getValue(nextProps, 'outcome.lastPricePercent');
+    if (lastPricePercent !== nextPricePercent) {
+      const lastPricePercentChange = lastPricePercent > nextPricePercent ? OUTCOME_VALUE_INCREASED : OUTCOME_VALUE_DECREASED;
+      // const that = this;
+      // setInterval(function () {
+      //   const change = Math.random() > '0.5' ? OUTCOME_VALUE_INCREASED : OUTCOME_VALUE_DECREASED;
+        this.setState({
+          lastPricePercentChange//: change
+        });
+      // }, 1000);
+    }
+  }
+
+  render() {
+
+    const p = this.props;
+    const s = this.state;
+    return (
+      <div
+        key={p.outcome.id}
+        className="outcome"
+      >
+        {!!p.outcome.lastPricePercent &&
+        <ValueDenomination
+          className={classNames('outcome-price', {
+            'flash-green': s.lastPricePercentChange === OUTCOME_VALUE_INCREASED,
+            'flash-red': s.lastPricePercentChange === OUTCOME_VALUE_DECREASED
+          })}
+          {...p.outcome.lastPricePercent}
+          formatted={p.outcome.lastPricePercent.rounded}
+          formattedValue={p.outcome.lastPricePercent.roundedValue}
+        />
+        }
+        <span
+          data-tip
+          data-for={`outcome-name-tooltip-${p.outcome.marketID}-${p.outcome.id}`}
+          data-event="click focus"
+          className="outcome-name"
+        >
+          {p.outcome.name}
+        </span>
+        <ReactTooltip
+          id={`outcome-name-tooltip-${p.outcome.marketID}-${p.outcome.id}`}
+          type="dark"
+          effect="float"
+          place="top"
+          globalEventOff="click"
+        >
+          <span
+            data-tip
+            data-for={`outcome-name-tooltip-${p.outcome.marketID}-${p.outcome.id}`}
+            data-event="click focus"
+            className="tooltip-text"
+          >
+            {p.outcome.name}
+          </span>
+        </ReactTooltip>
+      </div>
+    );
+  }
+}
--- src/modules/market/components/market-preview-outcomes.jsx
+++ src/modules/market/components/market-preview-outcomes.jsx
@@ -1,54 +1,17 @@
 import React from 'react';
-import ReactTooltip from 'react-tooltip';
-import ValueDenomination from 'modules/common/components/value-denomination';
+
+import MarketPreviewOutcome from 'modules/market/components/market-preview-outcome';
 
 const MarketOutcomes = p => (
   <div className="market-preview-outcomes">
     {(p.outcomes || []).map((outcome, i) => (
-      <div
-        key={outcome.id}
-        className="outcome"
-      >
-        {!!outcome.lastPricePercent &&
-          <ValueDenomination
-            className="outcome-price"
-            {...outcome.lastPricePercent}
-            formatted={outcome.lastPricePercent.rounded}
-            formattedValue={outcome.lastPricePercent.roundedValue}
-          />
-        }
-        <span
-          data-tip
-          data-for={`outcome-name-tooltip-${outcome.marketID}-${outcome.id}`}
-          data-event="click focus"
-          className="outcome-name"
-        >
-          {outcome.name}
-        </span>
-        <ReactTooltip
-          id={`outcome-name-tooltip-${outcome.marketID}-${outcome.id}`}
-          type="dark"
-          effect="float"
-          place="top"
-          globalEventOff="click"
-        >
-          <span
-            data-tip
-            data-for={`outcome-name-tooltip-${outcome.marketID}-${outcome.id}`}
-            data-event="click focus"
-            className="tooltip-text"
-          >
-            {outcome.name}
-          </span>
-        </ReactTooltip>
-      </div>
+      <MarketPreviewOutcome outcome={outcome} />
     ))}
   </div>
 );
 
-// TODO -- Prop Validations
-// MarketOutcomes.propTypes = {
-//  outcomes: React.PropTypes.array
-// };
+MarketOutcomes.propTypes = {
+  outcomes: React.PropTypes.array
+};
 
 export default MarketOutcomes;
