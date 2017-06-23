0e0b5a4540a090169e4af9ab8c1c0f7a144ea44d
JohnAllen <johnjalleniii@gmail.com>
2017-06-23 13:44:17 -0700

    Add uport logo component

---
+++ src/modules/common/components/uport-logo-icon.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const styles = {
+  margin: 'auto',
+  backgroundColor: 'green',
+  borderRadius: '3px'
+};
+
+const UPortLogoIcon = () => (
+  <img
+    style={styles}
+    className="uport-logo-icon"
+    alt="uPort Logo"
+    src="../../assets/images/uport-logo-white.svg"
+  />
+);
+
+export default UPortLogoIcon;
