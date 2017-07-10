0446925bb1b6c347c3b807d32feec93d11146096
John Danz <John.Danz87@gmail.com>
2017-07-10 17:32:52 -0500

    continued work on new onboarding screen

--- src/modules/auth/components/auth-onboard.jsx
+++ src/modules/auth/components/auth-onboard.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+// import PropTypes from 'prop-types';
+// import classNames from 'classnames';
 
 const AuthOnBoard = p => (
   <article className="auth-onboard">
-    <h1>Link an Ethereum Account to trade on anything from anywhere</h1>
+    <h1>Link an Ethereum Account <br />to trade on anything from anywhere</h1>
     <div className="auth-onboard-actions">
-      <button>Connect Account</button>
-      <button>Create Account</button>
-      <span>I already have an account with Ethereum currency in it</span>
-      <span>I need a new account to trade with Augur</span>
+      <div>
+        <button>Connect Account</button>
+        <span>I already have an account with Ethereum currency in it</span>
+      </div>
+      <div>
+        <button>Create Account</button>
+        <span>I need a new account to trade with Augur</span>
+      </div>
     </div>
   </article>
 );
